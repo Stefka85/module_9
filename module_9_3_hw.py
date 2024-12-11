70851bc7abbd284e04857849a9be1ee39355826e
Stefka85 <stephan.kourilo@gmail.com>
2024-12-11 08:42:16 +0300

    module_9_3_hw.py

--- module_9_3_hw.py
+++ module_9_3_hw.py
@@ -2,7 +2,7 @@ first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
 first_result = (len(i) - len(j) for i, j in zip(first, second) if len(i) != len(j))
-second_result = (len(first[i]) <= len(second[j]) for i in range(len(first)) for j in range(len(second)) if i == j)
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
 
 print(list(first_result))
 print(list(second_result))
